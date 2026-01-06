b45e1dca33a7cadeb5c594da53930dcf62d49765
Gerardo <gerardogomc99@gmail.com>
2026-01-05 22:07:45 -0600

    Refactor temperature measurement function and add forecast printing logic

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -253,7 +253,7 @@ console.log(`Total rechazados: ${totalRejects}`);
 console.log(`Mayor score: ${maxScoreDelivery}`);
 console.log(`Menor score: ${lessScoreDelivery}`); */
 
-const measureKelvin = function () {
+/* const measureKelvin = function () {
     const measurement = {
         type: 'tem',
         unit: 'celsius',
@@ -267,4 +267,19 @@ const measureKelvin = function () {
     return kelvin;
 }
 // A) Identify the bug
-console.log(measureKelvin());
+console.log(measureKelvin()); */
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log(`...${data1[0]}~C ... ${data1[1]}~C ... ${data1[2]}~C ...`);
+
+const printForecast = arr => {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+        str = str + `${arr[i]}~C...`;
+    }
+    console.log(str);
+}
+
+printForecast(data1)
