99b62af3321d291d8f84923bc043852e6db196f4
Gerardo <gerardogomc99@gmail.com>
2026-01-06 20:00:19 -0600

    Comment out debugging code and event listener for clarity

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+/* // Para ver el contenido en texto
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct number!';
+console.log(document.querySelector('.message').textContent);
+
+const score = document.querySelector('.score').textContent
+console.log(score);
+
+
+// Para ver el valor
+const guess = document.querySelector('.guess').value;
+console.log(guess);
+document.querySelector('.number').textContent = guess;
+
+ */
+
+/* 
+document.querySelector('.check')
+    .addEventListener('click', () => {
+        const guess = document.querySelector('.guess').value;
+        document.querySelector('.number').textContent = guess;
+    }); */
