3946d73f32f287e476de656aae5558b28332fb93
Gerardo <gerardogomc99@gmail.com>
2026-01-06 22:05:25 -0600

    Implement game logic for number guessing and update score handling

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -23,3 +23,44 @@ document.querySelector('.check')
         const guess = document.querySelector('.guess').value;
         document.querySelector('.number').textContent = guess;
     }); */
+
+const secretNumber =
+    Math.trunc(Math.random() * 20) + 1;
+
+const numberToGuess =
+    document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+
+const button = document.querySelector('.check')
+    .addEventListener('click', () => {
+        const adivinar = Number(document.querySelector('.guess').value);
+        const message = document.querySelector('.message');
+        const scoreInput = document.querySelector('.score');
+
+        if (!adivinar) {
+            message.textContent = 'NO NUMBER!';
+        } else if (adivinar === secretNumber) {
+            message.textContent = `THAT'S CORRECT!`;
+        } else if (adivinar < secretNumber) {
+            if (score > 1) {
+                message.textContent = `The number to guess is higher!`;
+                score--;
+                scoreInput.textContent = score;
+            } else {
+                message.textContent = 'You lose the game.!';
+                scoreInput.textContent = 0;
+            }
+        } else if (adivinar > secretNumber) {
+            if (score > 1) {
+                message.textContent = 'The number to guess is lower!';
+                score--;
+                scoreInput.textContent = score;
+            } else {
+                message.textContent = 'You lose the game.!';
+                scoreInput.textContent = 0;
+            }
+        }
+
+    });
