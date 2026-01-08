1c4b3439dc804856af5c24bbc943cfbddbb1dc4c
Gerardo <gerardogomc99@gmail.com>
2026-01-07 22:23:14 -0600

    Refactor modal functionality to improve event handling and keydown logic

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,8 +5,33 @@ const overlay = document.querySelector('.overlay');
 const show = document.querySelectorAll('.show-modal');
 const close = document.querySelector('.close-modal');
 
-console.log(show);
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
 
 for (let i = 0; i < show.length; i++) {
-    console.log(show[i].textContent);
+    show[i].addEventListener('click', openModal)
 }
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+close.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ESC' && !modal.classList.contains('hidden')) {
+        closeModal
+    }
+})
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        if (!modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    }
+})
