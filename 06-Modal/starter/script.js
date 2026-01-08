3dbd0a922310c6de4d155228e23abb8c59d8aaad
Gerardo <gerardogomc99@gmail.com>
2026-01-07 21:26:47 -0600

    Add initial modal setup and logging for show elements

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+// TEST
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay');
+const show = document.querySelectorAll('.show-modal');
+const close = document.querySelector('.close-modal');
+
+console.log(show);
+
+for (let i = 0; i < show.length; i++) {
+    console.log(show[i].textContent);
+}
