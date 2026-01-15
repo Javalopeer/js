6862327636544334768896d524cb8be4d0e2b4f1
Gerardo <gerardogomc99@gmail.com>
2026-01-14 18:05:00 -0600

    Initialize scores and display dice in the game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+const score1 = document.querySelector('#score--0')
+const score2 = document.querySelector('#score--1')
+const dice = document.querySelector('.dice')
+
+score1.textContent = 0
+score2.textContent = 0
+
+dice.classList.remove('hidden')
