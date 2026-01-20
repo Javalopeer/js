6326b6930f554514192f76a050df5342ac156cef
Javalopeer <gerardogomc99@gmail.com>
2026-01-19 22:01:34 -0600

    Refactor HTML doctype and enhance element selection in JavaScript for improved clarity

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,24 +3,35 @@
 // Selecting elements
 const score0 = document.querySelector('#score--0');
 const score1 = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const newGame = document.querySelector('.btn--new');
 const holdDice = document.querySelector('.btn--hold');
 
 
+
+
+
 //Starting conditions
 score0.textContent = 0
 score1.textContent = 0
 diceEl.classList.add('hidden')
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', () => {
     //  1. Generating random dice roll.
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
 
     //  2. Display the dice
     diceEl.src = `dice-${dice}.png`;
@@ -29,9 +40,16 @@ btnRoll.addEventListener('click', () => {
     //  3. Check for rolled 1
     if (dice !== 1) {
         // Add dice to current score
-
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // switch next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+
     }
 
 })
