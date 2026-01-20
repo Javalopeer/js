0b12f40e13904592ce3d002f26ee3719613ebd4d
Javalopeer <gerardogomc99@gmail.com>
2026-01-19 18:54:25 -0600

    Refactor element selection and initialization for improved clarity and organization

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,9 +1,37 @@
 'use strict';
-const score1 = document.querySelector('#score--0')
-const score2 = document.querySelector('#score--1')
-const dice = document.querySelector('.dice')
 
+// Selecting elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const newGame = document.querySelector('.btn--new');
+const holdDice = document.querySelector('.btn--hold');
+
+
+//Starting conditions
+score0.textContent = 0
 score1.textContent = 0
-score2.textContent = 0
+diceEl.classList.add('hidden')
+let currentScore = 0;
 
-dice.classList.remove('hidden')
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', () => {
+    //  1. Generating random dice roll.
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+
+    //  2. Display the dice
+    diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
+
+    //  3. Check for rolled 1
+    if (dice !== 1) {
+        // Add dice to current score
+
+    } else {
+        // switch next player
+    }
+
+})
