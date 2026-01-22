c4cd728d86c4049eef0140931732df09b48ee42d
Javalopeer <gerardogomc99@gmail.com>
2026-01-21 21:22:11 -0600

    Refactor restaurant object and order function for improved clarity

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,26 +1,26 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// // Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-const italianFoods = new Set([
-  'pasta',
-  'gnocchi',
-  'tomatoes',
-  'olive oil',
-  'garlic',
-  'basil',
-]);
+// const italianFoods = new Set([
+//   'pasta',
+//   'gnocchi',
+//   'tomatoes',
+//   'olive oil',
+//   'garlic',
+//   'basil',
+// ]);
 
-const mexicanFoods = new Set([
-  'tortillas',
-  'beans',
-  'rice',
-  'tomatoes',
-  'avocado',
-  'garlic',
-]);
+// const mexicanFoods = new Set([
+//   'tortillas',
+//   'beans',
+//   'rice',
+//   'tomatoes',
+//   'avocado',
+//   'garlic',
+// ]);
 
 // Data needed for first part of the section
 const restaurant = {
@@ -30,18 +30,58 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-};
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex],
+    this.mainMenu[mainIndex]]
+  }
+}
+// openingHours: {
+//   thu: {
+//     open: 12,
+//       close: 22,
+//     },
+//   fri: {
+//     open: 11,
+//       close: 23,
+//     },
+//   sat: {
+//     open: 0, // Open 24 hours
+//       close: 24,
+//     },
+// },
+// };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+// function getCoordinates() {
+//   return [9.93, -84.08];;
+// }
+
+// const [lat, lng] = getCoordinates();
+
+// console.log(lat, lng);
+
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[secondary, main] = [main, secondary];
+console.log(main, secondary);
+
+const array = (assignment) => console.log(...assignment);
+const numbers = [1, 2, 3];
+array(numbers);
+
+const complete = [...numbers, 4, 5];
+console.log(...complete);
+
+
+const [starter, mainCourse] = (restaurant.order(2, 0));
+console.log(`${starter} and ${mainCourse}`);
